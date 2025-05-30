c426a3cb60b2da2a1adac6dffcb3f45a3de9e43c
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2025-05-30 11:52:19 -0300

    mudando a estrutura para laravel 11 e criando migrations

---
+++ emma/database/migrations/2025_05_30_141756_create_report_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id('id_report')->primary();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('employee_id');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report');
+    }
+};
